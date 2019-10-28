14d127f7006b8d0d3ff102f12ebe07321d68fae7
Jatuwatsag <thehandsomeify@gmail.com>
2019-10-28 10:27:55 +0700

    build(website): Migrate to vue framework

---
+++ iot-website/src/views/Home.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="home">
+    <div class="row justify-content-md-center">
+      <img
+        src="https://onefact.net/wp-content/uploads/2016/06/seniorproject.png"
+        class="img-fluid"
+        alt="Responsive image"
+        style="width:50%;height:50%;margin:20px;"
+      />
+      <div class="col-12">
+        <!-- Set the number of people -->
+        <h2>
+          Current number of people:
+          <b>{{ currentNumberOfPeople }}</b>
+        </h2>
+      </div>
+      <div class="input-group col-12">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="# of people"
+          v-model="numberOfPeople"
+          style="color:black;font-size:50px;"
+        />
+        <div class="input-group-append" id="button-addon4">
+          <button
+            class="btn btn-success"
+            type="button"
+            data-toggle="modal"
+            data-target="#confirmSetModal"
+            :disabled="resetBtnDisable"
+            style="paddingLeft:25px;paddingRight:25px;"
+          >
+            <b>SET</b>
+          </button>
+          <button
+            class="btn btn-secondary"
+            type="button"
+            data-toggle="modal"
+            data-target="#confirmResetModal"
+            :disabled="resetBtnDisable"
+          >
+            Reset Background
+            <br />
+            <h5 v-if="resetBtnIsClicked">(10s)</h5>
+          </button>
+        </div>
+      </div>
+      <!-- Reset button loading -->
+      <div class="d-flex align-items-center" v-if="resetBtnDisable" style="margin:10px">
+        <strong>Processing...</strong>
+        <div class="spinner-border ml-auto" role="status" aria-hidden="true" style="margin:20px;"></div>
+      </div>
+
+      <!-- Table -->
+      <div class="col-12">
+        <hr />
+        <h2>Data Table</h2>
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">
+                Date &amp;
+                <br />Time
+              </th>
+              <th scope="col">
+                Number of
+                <br />People
+              </th>
+              <th scope="col">
+                CO2
+                <br />(ppm)
+              </th>
+              <th scope="col">
+                Hum
+                <br />(%)
+              </th>
+              <th scope="col">
+                Temp
+                <br />(*C)
+              </th>
+              <th scope="col">
+                Light
+                <br />(LUX)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">12:01:00</th>
+              <td>12</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+            </tr>
+            <tr>
+              <th scope="row">12:00:30</th>
+              <td>11</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+            </tr>
+            <tr>
+              <th scope="row">12:00:00</th>
+              <td>11</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+            </tr>
+            <tr>
+              <th scope="row">11:59:30</th>
+              <td>9</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+              <td>XXX</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <!-- Modal for Set button -->
+      <div class="modal fade" id="confirmSetModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>
+                You're setting from
+                <b>{{currentNumberOfPeople}}</b> people to
+                <b>{{numberOfPeople}}</b> people
+              </p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-dismiss="modal"
+                @click="setNumberOfPeople"
+              >Confirm!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal for Reset button -->
+      <div class="modal fade" id="confirmResetModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Wait for 10 seconds to reset the background.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
+              <button
+                type="button"
+                class="btn btn-danger"
+                data-dismiss="modal"
+                v-on:click="resetNumberOfPeople()"
+              >Reset</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+// @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Home extends Vue {
+  private numberOfPeople = 0;
+  private currentNumberOfPeople = 12;
+  private resetBackgroundTimer = 10;
+  private resetBtnIsClicked = false;
+  private resetBtnDisable = false;
+
+  private async setNumberOfPeople() {
+    if (this.numberOfPeople < 0) {
+      alert(
+        "Please input valid number" +
+          "\n" +
+          "The number must be equal or more than 0"
+      );
+    } else {
+      console.log("People: " + this.numberOfPeople);
+    }
+
+    const result = await this.axios.post(
+      "http://localhost:3000/test",
+      this.numberOfPeople
+    );
+    console.log(result.data);
+  }
+
+  private resetNumberOfPeople() {
+    this.resetBtnIsClicked = true;
+    this.resetBtnDisable = true;
+    setTimeout(() => {
+      this.resetBtnIsClicked = false;
+      this.resetBtnDisable = false;
+      alert("Background reset successful!");
+    }, 10000);
+  }
+}
+</script>
